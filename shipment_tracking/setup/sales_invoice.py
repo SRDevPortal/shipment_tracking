cb544e8811a87a51f20c023b2d398061dba66159
SRDevPortal <webdevelopersriaas@gmail.com>
2026-04-03 12:29:54 +0530

    Update sales invoice setup

--- shipment_tracking/setup/sales_invoice.py
+++ shipment_tracking/setup/sales_invoice.py
@@ -1,15 +1,14 @@
 from .utils import create_cf_with_module
 
-
-PARENT = "Sales Invoice"
+DT = "Sales Invoice"
 
 
 def apply():
     create_cf_with_module(
         {
-            PARENT: [
+            DT: [
                 {
-                    "fieldname": "source_encounter",
+                    "fieldname": "si_source_encounter",
                     "label": "Source Encounter",
                     "fieldtype": "Link",
                     "options": "Patient Encounter",
@@ -17,57 +16,64 @@ def apply():
                     "insert_after": "patient_name",
                 },
                 {
-                    "fieldname": "shipment_tracking_section",
+                    "fieldname": "si_shipment_tracking_tab",
                     "label": "Shipment Tracking",
+                    "fieldtype": "Tab Break",
+                    "insert_after": "terms",
+                },
+                {
+                    "fieldname": "si_shipment_tracking_section",
                     "fieldtype": "Section Break",
-                    "insert_after": "sr_si_delivery_type",
+                    "insert_after": "si_shipment_tracking_tab",
                     "collapsible": 1,
                 },
                 {
-                    "fieldname": "shipkia_order_id",
+                    "fieldname": "si_shipkia_order_id",
                     "label": "Shipkia Order ID",
                     "fieldtype": "Data",
                     "read_only": 1,
                     "in_list_view": 1,
-                    "insert_after": "shipment_tracking_section",
+                    "in_standard_filter": 1,
+                    "insert_after": "si_shipment_tracking_section",
                 },
                 {
-                    "fieldname": "shipkia_awb_number",
+                    "fieldname": "si_shipkia_awb_number",
                     "label": "Shipkia AWB Number",
                     "fieldtype": "Data",
                     "read_only": 1,
                     "in_list_view": 1,
-                    "insert_after": "shipkia_order_id",
+                    "insert_after": "si_shipkia_order_id",
                 },
                 {
-                    "fieldname": "shipkia_status",
+                    "fieldname": "si_shipkia_status",
                     "label": "Shipkia Status",
                     "fieldtype": "Data",
                     "read_only": 1,
                     "in_list_view": 1,
-                    "insert_after": "shipkia_awb_number",
+                    "in_standard_filter": 1,
+                    "insert_after": "si_shipkia_awb_number",
                 },
                 {
-                    "fieldname": "shipkia_estimated_delivery",
-                    "label": "Shipkia Estimated Delivery",
+                    "fieldname": "si_shipkia_estimated_delivery",
+                    "label": "Estimated Delivery",
                     "fieldtype": "Datetime",
                     "read_only": 1,
-                    "insert_after": "shipkia_status",
+                    "insert_after": "si_shipkia_status",
                 },
                 {
-                    "fieldname": "shipkia_delivered_on",
-                    "label": "Shipkia Delivered On",
+                    "fieldname": "si_shipkia_delivered_on",
+                    "label": "Delivered On",
                     "fieldtype": "Datetime",
                     "read_only": 1,
-                    "insert_after": "shipkia_estimated_delivery",
+                    "insert_after": "si_shipkia_estimated_delivery",
                 },
                 {
-                    "fieldname": "shipkia_shipment",
+                    "fieldname": "si_shipkia_shipment",
                     "label": "Shipment Record",
                     "fieldtype": "Link",
                     "options": "Shipment Tracking Shipment",
                     "read_only": 1,
-                    "insert_after": "shipkia_delivered_on",
+                    "insert_after": "si_shipkia_delivered_on",
                 },
             ]
         }
