55a9f2a1ccfe434778a9a5e7ef11ba2ef9b92441
SRDevPortal <webdevelopersriaas@gmail.com>
2026-04-15 12:33:47 +0530

    Updated icons JS logic

--- shipment_tracking/public/js/icons.js
+++ shipment_tracking/public/js/icons.js
@@ -1,9 +1,25 @@
-frappe.ready(() => {
+function injectShipmentIcons() {
+    if (document.getElementById("shipment-tracking-icons")) {
+        return;
+    }
+
     fetch("/assets/shipment_tracking/icons/shipment_icons.svg")
-        .then(res => res.text())
-        .then(svg => {
+        .then((res) => res.text())
+        .then((svg) => {
             const div = document.createElement("div");
+            div.id = "shipment-tracking-icons";
             div.innerHTML = svg;
             document.body.prepend(div);
+        })
+        .catch((err) => {
+            console.warn("Shipment icons could not be loaded.", err);
         });
-});
+}
+
+if (typeof frappe !== "undefined" && typeof frappe.ready === "function") {
+    frappe.ready(injectShipmentIcons);
+} else if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", injectShipmentIcons, { once: true });
+} else {
+    injectShipmentIcons();
+}
