b4d1d6814c94600efb67518f56b967f122ddfe50
SRDevPortal <webdevelopersriaas@gmail.com>
2026-04-29 13:22:04 +0530

    Enhance sales invoice linkage

--- shipment_tracking/setup/sales_invoice.py
+++ shipment_tracking/setup/sales_invoice.py
@@ -1,4 +1,6 @@
-from .utils import create_cf_with_module
+import frappe
+
+from .utils import create_cf_with_module, delete_custom_fields, migrate_custom_field_data
 
 DT = "Sales Invoice"
 
@@ -8,7 +10,7 @@ def apply():
         {
             DT: [
                 {
-                    "fieldname": "si_source_encounter",
+                    "fieldname": "source_encounter",
                     "label": "Source Encounter",
                     "fieldtype": "Link",
                     "options": "Patient Encounter",
@@ -45,13 +47,21 @@ def apply():
                     "insert_after": "si_shipkia_order_id",
                 },
                 {
+                    "fieldname": "si_shipkia_stage",
+                    "label": "Shipkia Stage",
+                    "fieldtype": "Data",
+                    "read_only": 1,
+                    "in_list_view": 1,
+                    "insert_after": "si_shipkia_awb_number",
+                },
+                {
                     "fieldname": "si_shipkia_status",
                     "label": "Shipkia Status",
                     "fieldtype": "Data",
                     "read_only": 1,
                     "in_list_view": 1,
                     "in_standard_filter": 1,
-                    "insert_after": "si_shipkia_awb_number",
+                    "insert_after": "si_shipkia_stage",
                 },
                 {
                     "fieldname": "si_shipkia_estimated_delivery",
@@ -68,13 +78,36 @@ def apply():
                     "insert_after": "si_shipkia_estimated_delivery",
                 },
                 {
+                    "fieldname": "si_delivery_partner",
+                    "label": "Courier Partner",
+                    "fieldtype": "Data",
+                    "read_only": 1,
+                    "in_list_view": 1,
+                    "insert_after": "si_shipkia_delivered_on",
+                },
+                {
                     "fieldname": "si_shipkia_shipment",
                     "label": "Shipment Record",
                     "fieldtype": "Link",
                     "options": "Shipment Tracking Shipment",
                     "read_only": 1,
-                    "insert_after": "si_shipkia_delivered_on",
+                    "insert_after": "si_delivery_partner",
                 },
             ]
         }
     )
+
+    cleanup_duplicate_fields()
+
+
+def cleanup_duplicate_fields():
+    migrate_custom_field_data(DT, "si_source_encounter", "source_encounter")
+    migrate_custom_field_data(DT, "si_shipkia_order_stage", "si_shipkia_stage")
+
+    obsolete_fields = []
+    if frappe.db.has_column(DT, "source_encounter"):
+        obsolete_fields.append("si_source_encounter")
+    if frappe.db.has_column(DT, "si_shipkia_stage"):
+        obsolete_fields.append("si_shipkia_order_stage")
+
+    delete_custom_fields(DT, obsolete_fields)
